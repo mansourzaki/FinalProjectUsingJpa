f265a47ad610debd9e12a48d258ae68074abced5
mansourzaki <43302116+mansourzaki@users.noreply.github.com>
2021-06-20 22:11:13 +0300

    Update Users.java

--- src/Entities/Users.java
+++ src/Entities/Users.java
@@ -17,10 +17,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- *
- * @author manso
- */
+
 @Entity
 @Table(name = "users")
 @XmlRootElement
